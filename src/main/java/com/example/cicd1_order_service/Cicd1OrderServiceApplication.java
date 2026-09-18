6fe4a15de313fe43476e748ff33219c4d5abce90
PaulDaly-STUDENT <g00470372@atu.ie>
2026-09-18 10:01:54 +0100

    Initial Order Service Commit

---
+++ src/main/java/com/example/cicd1_order_service/Cicd1OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.cicd1_order_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cicd1OrderServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cicd1OrderServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cicd1_order_service/Cicd1OrderServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cicd1_order_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cicd1OrderServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
